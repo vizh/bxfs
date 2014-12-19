0152e3e1234223a9fee50a4cf8fd336f196eb7cb
Максим Махнюк <star.absorber@gmail.com>
2014-12-19 09:30:08 +0100

    Closes #4601: Исправлен архитектурный недочёт. Нельзя было использовать имплементирующий PsiReference класс для расширения CompletionContributor.

--- src/pro/opcode/bitrix/references/BxComponentTemplateReference.java
+++ src/pro/opcode/bitrix/references/BxComponentTemplateReference.java
@@ -1,17 +1,9 @@
 package pro.opcode.bitrix.references;
 
-import com.intellij.codeInsight.completion.CompletionParameters;
-import com.intellij.codeInsight.completion.CompletionProvider;
-import com.intellij.codeInsight.completion.CompletionResultSet;
-import com.intellij.codeInsight.completion.CompletionType;
-import com.intellij.codeInsight.lookup.LookupElementBuilder;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
-import com.intellij.util.ProcessingContext;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import pro.opcode.bitrix.BxReference;
-import pro.opcode.bitrix.BxReferencePatterns;
 import pro.opcode.bitrix.api.BxComponent;
 
 import java.util.Arrays;
@@ -27,23 +19,23 @@ public class BxComponentTemplateReference extends BxReference
 		 * Построение автокомплита компонента в процессе набора $APPLICATION->IncludeComponent("component", "...")
 		 */
 
-	public BxComponentTemplateReference() {
-		extend(CompletionType.BASIC, BxReferencePatterns.bxComponentReference(), new CompletionProvider<CompletionParameters>() {
-			public void addCompletions(@NotNull CompletionParameters parameters, ProcessingContext context, @NotNull CompletionResultSet resultSet) {
+//	public BxComponentTemplateReference() {
+//		extend(CompletionType.BASIC, BxReferencePatterns.bxComponentReference(), new CompletionProvider<CompletionParameters>() {
+//			public void addCompletions(@NotNull CompletionParameters parameters, ProcessingContext context, @NotNull CompletionResultSet resultSet) {
 //				BxCore bitrix = new BxCore(parameters.getPosition().getProject());
-
+//
 //				if (!bitrix.isComponentsFolderExists())
-					for (String component : bxCommonTemplates)
-						resultSet.addElement(LookupElementBuilder.create("bitrix:" + component));
-
+//					for (String component : bxCommonTemplates)
+//						resultSet.addElement(LookupElementBuilder.create("bitrix:" + component));
+//
 //				for (VirtualFile component : bitrix.getComponents())
 //					resultSet.addElement(LookupElementBuilder.create(String.format("%s:%s",
 //						component.getParent().getName(),
 //						component.getName()
 //					)));
-			}
-		});
-	}
+//			}
+//		});
+//	}
 
 	public BxComponentTemplateReference(PsiElement element) {
 		super(element);
